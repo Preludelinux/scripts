c45e468bd1ed86d589aa1cb64673012c1c554733
Noah J Norris <preludelinux@gmail.com>
2012-04-24 18:33:25 -0500

    Added kicad scripts to pull many lib packages TODO add all files into 1 or 2 big zipped packages

---
+++ kicadLIBS.sh
@@ -0,0 +1,247 @@
+cd Downloads/KICADLIBS/
+wget http://www.kicadlib.org/modules/lm339.lib.zip
+wget http://www.kicadlib.org/modules/gajda_analogsw_lib.zip
+wget http://www.kicadlib.org/modules/ad594-95.lib.zip
+wget http://www.kicadlib.org/modules/lm4674.zip
+wget http://www.kicadlib.org/modules/gajda_amplif_lib.zip
+wget http://www.kicadlib.org/modules/BW_Capacitors_ThroughHole_RevA.zip
+wget http://www.kicadlib.org/modules/BW_Capacitors_SMD_RevA.zip
+wget http://www.kicadlib.org/modules/BW_Elko_ThroughHole_RevA.zip
+wget http://www.kicadlib.org/modules/AP_Epcos-MKT.lib.zip
+wget http://www.kicadlib.org/modules/AP_Epcos-MKT.mod.zip
+wget http://www.kicadlib.org/modules/vs10xx.lib.zip
+wget http://www.kicadlib.org/modules/fcp_zif_18.mod.zip
+wget http://www.kicadlib.org/modules/fpc_conn_zif_smt_18.wrl.zip
+wget http://www.kicadlib.org/modules/usb-b.lib.zip
+wget http://www.kicadlib.org/modules/conn_usb_b_emp.zip
+wget http://www.kicadlib.org/modules/conn_usb-b_3d.zip
+wget http://www.kicadlib.org/modules/3m_serie_2500_smd.mod.zip
+wget http://www.kicadlib.org/modules/cnt-rj45.lib.zip
+wget http://www.kicadlib.org/modules/cnt-rj45.emp.zip
+wget http://www.kicadlib.org/modules/snap-imsys72.lib.zip
+wget http://www.kicadlib.org/modules/sma_connectors.zip
+wget http://www.kicadlib.org/modules/sma_connectors.zip
+wget http://www.kicadlib.org/modules/connectors_omron.mod.zip
+wget http://www.kicadlib.org/modules/centronics_f.lib.zip
+wget http://www.kicadlib.org/modules/centronics_f.mod.zip
+wget http://www.kicadlib.org/modules/Socket-DIN41612_RevA.lib.zip
+wget http://www.kicadlib.org/modules/BW_Mechanical_Socket-Plug_DIN41612-Stuff_RevA.zip
+wget http://www.kicadlib.org/modules/JOS_SMA_connectors.zip
+wget http://www.kicadlib.org/modules/d_pack.lib.zip
+wget http://www.kicadlib.org/modules/transil_diode.lib.zip
+wget http://www.kicadlib.org/modules/BW_Dioden_ThroughHole_RevC.zip
+wget http://www.kicadlib.org/modules/BW_Dioden_SMD_RevA_27July2010.zip
+wget http://www.kicadlib.org/modules/s5038.lib.zip
+wget http://www.kicadlib.org/modules/s5038.mod.zip
+wget http://www.kicadlib.org/modules/s5038.3d.zip
+wget http://www.kicadlib.org/modules/vocanson_display_lib.zip
+wget http://www.kicadlib.org/modules/vocanson_display_mod.zip
+wget http://www.kicadlib.org/modules/charlie_display_lcd.zip
+wget http://www.kicadlib.org/modules/line_drivers.lib.zip
+wget http://www.kicadlib.org/modules/75lbc179_lib.zip
+wget http://www.kicadlib.org/modules/stepper_drivers.lib.zip
+wget http://www.kicadlib.org/modules/YT_borniers.zip
+wget http://www.kicadlib.org/modules/galva.lib.zip
+wget http://www.kicadlib.org/modules/RelaisFinder4052.zip
+wget http://www.kicadlib.org/modules/relay-v23092.lib.zip
+wget http://www.kicadlib.org/modules/relay-v23092.emp.zip
+wget http://www.kicadlib.org/modules/dips-s.lib.zip
+wget http://www.kicadlib.org/modules/dips-s.mod.zip
+wget http://www.kicadlib.org/modules/dips-s_3d.zip
+wget http://www.kicadlib.org/modules/lib_switch_dip.zip
+wget http://www.kicadlib.org/modules/clav12.zip
+wget http://www.kicadlib.org/modules/clav12.zip
+wget http://www.kicadlib.org/modules/clav12.zip
+wget http://www.kicadlib.org/modules/YT_electrotechnique.zip
+wget http://www.kicadlib.org/modules/SHOGYO%20export.zip
+wget http://www.kicadlib.org/modules/C&K%20export.zip
+wget http://www.kicadlib.org/modules/E-SWITCH%20export.zip
+wget http://www.kicadlib.org/modules/ITW%20export.zip
+wget http://www.kicadlib.org/modules/KNITTER-SWITCH%20export.zip
+wget http://www.kicadlib.org/modules/SWITCH%20CHANNEL%20export.zip
+wget http://www.kicadlib.org/modules/SWITRONIC%20export.zip
+wget http://www.kicadlib.org/modules/TONELUCK%20export.zip
+wget http://www.kicadlib.org/modules/WELL%20BUYING%20export.zip
+wget http://www.kicadlib.org/modules/BW_BNC-Sockets_RevA.zip
+wget http://www.kicadlib.org/modules/BW_Buzzer_Beeper_RevA_25Oct2010.zip
+wget http://www.kicadlib.org/modules/GR_relays.lib.zip
+wget http://www.kicadlib.org/modules/GR_relays.mod.zip
+wget http://www.kicadlib.org/modules/BW_Heatsinks_RevC.zip
+wget http://www.kicadlib.org/modules/BW_IC-Socket_UniversalDIP8-SO8_RevB.zip
+wget http://www.kicadlib.org/modules/BW_Label_RevA_21Mar2011.zip
+wget http://www.kicadlib.org/modules/BW_Measurement_Point_RevA.zip
+wget http://www.kicadlib.org/modules/BW_Measurement-Scala_RevA.zip
+wget http://www.kicadlib.org/modules/sc16is7x2_spi_tssop28_lib.zip
+wget http://www.kicadlib.org/modules/tssoic28_mod.zip
+wget http://www.kicadlib.org/modules/16c754_80_lib.zip
+wget http://www.kicadlib.org/modules/vqfp80_emp.zip
+wget http://www.kicadlib.org/modules/vqfp80_wrl.zip
+wget http://www.kicadlib.org/modules/ft232bm.lib.zip
+wget http://www.kicadlib.org/modules/ft245bm_lib.zip
+wget http://www.kicadlib.org/modules/mm232r.lib.zip
+wget http://www.kicadlib.org/modules/mm232r.mod.zip
+wget http://www.kicadlib.org/modules/io_warrior.lib.zip
+wget http://www.kicadlib.org/modules/mt8889.lib.zip
+wget http://www.kicadlib.org/modules/gajda_ttlieee.lib.zip
+wget http://www.kicadlib.org/modules/gajda_cmosieee.lib.zip
+wget http://www.kicadlib.org/modules/cmos4000b.lib.zip
+wget http://www.kicadlib.org/modules/KK_74xgxx.zip
+wget http://www.kicadlib.org/modules/93lc46b.lib.zip
+wget http://www.kicadlib.org/modules/atmega8_lib.zip
+wget http://www.kicadlib.org/modules/atmega8_16pi_lib.zip
+wget http://www.kicadlib.org/modules/at89s52.lib.zip
+wget http://www.kicadlib.org/modules/tqfp32_08_emp.zip
+wget http://www.kicadlib.org/modules/atmega16.lib.zip
+wget http://www.kicadlib.org/modules/atmega8535.lib.zip
+wget http://www.kicadlib.org/modules/ATMEGA8535.emp.zip
+wget http://www.kicadlib.org/modules/at89s52.lib.zip
+wget http://www.kicadlib.org/modules/tqfp44_08_emp.zip
+wget http://www.kicadlib.org/modules/atmega64_lib.zip
+wget http://www.kicadlib.org/modules/tqfp64_08_emp.zip
+wget http://www.kicadlib.org/modules/attiny_lib.zip
+wget http://www.kicadlib.org/modules/VF_PIC18F13K50.zip
+wget http://www.kicadlib.org/modules/VF_PIC18F14K50.zip
+wget http://www.kicadlib.org/modules/pic18f2431.lib.zip
+wget http://www.kicadlib.org/modules/18f2550_lib.zip
+wget http://www.kicadlib.org/modules/pic18f4455_dip40.lib.zip
+wget http://www.kicadlib.org/modules/pic18f45j10_dip40.lib.zip
+wget http://www.kicadlib.org/modules/18f4550_lib.zip
+wget http://www.kicadlib.org/modules/pic18f4550_tqfp.lib.zip
+wget http://www.kicadlib.org/modules/NV_pic18f67j60.lib.zip
+wget http://www.kicadlib.org/modules/HB_pic18f97j60.lib.zip
+wget http://www.kicadlib.org/modules/HB_TQFP44.emp.zip
+wget http://www.kicadlib.org/modules/kk_nxp_armmcu.zip
+wget http://www.kicadlib.org/modules/lpc2138_lib.zip
+wget http://www.kicadlib.org/modules/lpc2148.lib.zip
+wget http://www.kicadlib.org/modules/lqfp64_05.emp.zip
+wget http://www.kicadlib.org/modules/p89c51.lib.zip
+wget http://www.kicadlib.org/modules/plcc-44-pin_3d.wrl.zip
+wget http://www.kicadlib.org/modules/str710.lib.zip
+wget http://www.kicadlib.org/modules/msp430.lib.zip
+wget http://www.kicadlib.org/modules/pic16f737.lib.zip
+wget http://www.kicadlib.org/modules/kk_atmel_library_.zip
+wget http://www.kicadlib.org/modules/ME_luminary.zip
+wget http://www.kicadlib.org/modules/Symbols_ICs-Diskrete_RevC.lib.zip
+wget http://www.kicadlib.org/modules/SymbolsSimilarEn60617-RevE4.lib.zip
+wget http://www.kicadlib.org/modules/GR_Chip_Card.lib.zip
+wget http://www.kicadlib.org/modules/GR_Chip_Card.mod.zip
+wget http://www.kicadlib.org/modules/BW_Fuseholder_RevD_24Feb2011.zip
+wget http://www.kicadlib.org/modules/BW_Footprint-Symbols_RevD.zip
+wget http://www.kicadlib.org/modules/BW_Fiducials_RevB.zip
+wget http://www.kicadlib.org/modules/IC_raspberry.lib
+wget http://www.kicadlib.org/modules/gajda_opto_lib.zip
+wget http://www.kicadlib.org/modules/BW_Opto-Devices_RevC_30Apr2011.zip
+wget http://www.kicadlib.org/modules/Symbols_ICs-Opto_RevA.lib.zip
+wget http://www.kicadlib.org/modules/cer_reso_lib.zip
+wget http://www.kicadlib.org/modules/resonator_3pins.emp.zip
+wget http://www.kicadlib.org/modules/quartz_x014s.png
+wget http://www.kicadlib.org/modules/quartz_x014s.zip
+wget http://www.kicadlib.org/modules/BW_Crystals_RevA_09Aug2010.zip
+wget http://www.kicadlib.org/modules/varistor_epcos.zip
+wget http://www.kicadlib.org/modules/varistor_epcos.zip
+wget http://www.kicadlib.org/modules/usbn9603.lib.zip
+wget http://www.kicadlib.org/modules/YT-potentiometres_3D.zip
+wget http://www.kicadlib.org/modules/YT_Potentiometres.mod.zip
+wget http://www.kicadlib.org/modules/YT-inductances.zip
+wget http://www.kicadlib.org/modules/YT-inductances_3D.zip
+wget http://www.kicadlib.org/modules/BW_Chokes.zip
+wget http://www.kicadlib.org/modules/BW_CommonModeChoke_Wuerth_Type-WE-CMB_RevA_25Oct2010.zip
+wget http://www.kicadlib.org/modules/d2pak_emp.zip
+wget http://www.kicadlib.org/modules/multiwatt11.mod.zip
+wget http://www.kicadlib.org/modules/gajda_power_mod.zip
+wget http://www.kicadlib.org/modules/gajda_power_3d.zip
+wget http://www.kicadlib.org/modules/kk_power_integrations.zip
+wget http://www.kicadlib.org/modules/kk_power_integrations.zip
+wget http://www.kicadlib.org/modules/Symbols_Transformer-Diskrete_RevA.lib.zip
+wget http://www.kicadlib.org/modules/BW_DCDC-Converter_RevB_14Jun2011.zip
+wget http://www.kicadlib.org/modules/BW_Pentawatt_RevB_30Apr2011.zip
+wget http://www.kicadlib.org/modules/BW_IR-directFET_Packages_RevA_30Jun2011.zip
+wget http://www.kicadlib.org/modules/sra1.zip
+wget http://www.kicadlib.org/modules/rfm70.zip
+wget http://www.kicadlib.org/modules/WZ_RS9110-N-11-22.zip
+wget http://www.kicadlib.org/modules/uln280xa.lib.zip
+wget http://www.kicadlib.org/modules/on_semiconductor.zip
+wget http://www.kicadlib.org/modules/cg_valves.lib.zip
+wget http://www.kicadlib.org/modules/Dispositivos_I2C.lib.zip
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ kicadlibs2.sh
@@ -0,0 +1,85 @@
+mkdir Downloads/KICADLIBS2
+cd Downloads/KICADLIBS2/
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_conn.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_power.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_w_analog.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_w_connectors.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_w_device.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_w_logic.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_w_microcontrollers.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_w_opto.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/eesch/lib_w_rtx.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_capacitors.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_capacitors.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_9159.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_9159.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_av.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_av.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_d-sub.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_d-sub.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_misc.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_misc.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_modu.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_modu.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_panelmate.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_panelmate.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_pc.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_pc.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_screw.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_screw.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_conn_strip.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_conn_strip.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_details.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_details.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_indicators.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_indicators.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_misc_comp.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_misc_comp.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_pin_strip.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_pin_strip.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_pth_circuits.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_pth_circuits.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_pth_diodes.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_pth_diodes.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_pth_plcc.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_pth_plcc.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_pth_resistors.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_pth_resistors.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_relay.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_relay.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_to.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_to.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_bga.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_bga.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_cap.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_cap.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_dil.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_dil.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_diode.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_diode.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_leds.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_leds.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_lqfp.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_lqfp.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_plcc.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_plcc.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_qfn.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_qfn.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_resistors.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_resistors.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/pcbnew/mod_smd_trans.zip
+wget http://smisioto.no-ip.org/elettronica/kicad/libs/3d/3d_smd_trans.zip
+
+
+
+
+
+
+
+
+
+
+
+
+
+
